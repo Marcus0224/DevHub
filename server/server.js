1061998a42537fbf933aaae7140951b0f5a532dd
Marcus0224 <marcstlouis04@gmail.com>
2022-05-30 19:57:05 -0400

    fix bugs

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dev-hub/build')));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dev-hub/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
