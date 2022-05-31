4a2d8458c257fc2d89dff5846e1437d32370206d
jrwj97 <jrwj97@aol.com>
2022-05-31 00:32:08 -0400

    commit begore switching bramch

--- dev-hub/src/index.js
+++ dev-hub/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import { ChakraProvider } from '@chakra-ui/react'
-import {NavBar} from './components/Navbar';
+import { NavBar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Cart } from './components/Cart';
 import { Login } from './components/Login';
